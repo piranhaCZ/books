4b01930e9bf98aab13a3d8d974f2b62023944029
piranha <novakova.martyna@gmail.com>
2020-07-12 11:19:16 +0200

    Django forms

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,21 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
+from forms.forms import UserForm
 
 
 def index(request):
-    context = {
-        'test': 'toto je test',
-    }
+    submitbutton = request.POST.get("submit")
+
+    bookname = ''
+    authorname = ''
+
+
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        bookname = form.cleaned_data.get("book_name")
+
+
+    context = {'form': form, 'bookname': bookname, 'authorname': authorname,
+               'submitbutton': submitbutton}
+
     return render(request, 'pages/index.html', context)
